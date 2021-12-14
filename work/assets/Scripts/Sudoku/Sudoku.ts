1aac8596d23e02186fdd774e83aaa7d965d3eeb7
Kirito870425 <dsa50163@gmail.com>
2021-12-14 16:42:19 +0800

    Sudoku
    九宮格、列、PlayerButton

--- work/assets/Scripts/Sudoku/Sudoku.ts
+++ work/assets/Scripts/Sudoku/Sudoku.ts
@@ -32,40 +32,136 @@ export default class Sudoku extends cc.Component {
         }
 
         for (let j = 0; j < this.InputIndex; j++) {
+
             this.createOfRule();
         }
+
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+            }
+            this.colArray.push(this.sudokuArray[this.sudokuTakeTheQuotient(j)][this.sudokuTakeTheRemainder(i)]);
+            // console.log("===j===" + j, "===i===" + i);
+            console.log("===i===" + i);
+        }
+        console.log("this.colArray===" + this.colArray);
+
+        // this.solveSudoku([["5", "3", ".", ".", "7", ".", ".", ".", "."], ["6", ".", ".", "1", "9", "5", ".", ".", "."], [".", "9", "8", ".", ".", ".", ".", "6", "."], ["8", ".", ".", ".", "6", ".", ".", ".", "3"], ["4", ".", ".", "8", ".", "3", ".", ".", "1"], ["7", ".", ".", ".", "2", ".", ".", ".", "6"], [".", "6", ".", ".", ".", ".", "2", "8", "."], [".", ".", ".", "4", "1", "9", ".", ".", "5"], [".", ".", ".", ".", "8", ".", ".", "7", "9"]]);
+        // console.log("=sudokuArray=" + this.sudokuArray);
+
     }
 
+
+    solveSudoku(board: string[][]): void {
+        const candidates = "123456789";
+        const isLegal = (row: number, col: number): boolean => {
+            const test = (strs: string[], target): boolean =>
+                strs.filter((val) => val === target).length === 1;
+
+
+            const target = board[row][col];
+
+            const rowItems = board[row];
+            if (!test(rowItems, target)) return false;
+
+            const colItems = board.map((_row) => _row[col]);
+            if (!test(colItems, target)) return false;
+
+            const boxRowStart = Math.floor(row / 3) * 3;
+            const boxColStart = Math.floor(col / 3) * 3;
+            const boxItems = board
+                .slice(boxRowStart, boxRowStart + 3)
+                .map((_row) => _row.slice(boxColStart, boxColStart + 3))
+                .reduce((prev, next) => [...prev, ...next], []);
+
+
+            if (!test(boxItems, target)) return false;
+
+            return true;
+        };
+
+        // 換行
+        const getNextPos = (row: number, col: number): [number, number] =>
+            col + 1 === 9 ? [row + 1, 0] : [row, col + 1];
+
+        let done = false;
+
+        const backtracking = (row: number, col: number): void => {
+            // backtracing boundary
+            if (row === 9) done = true; // test done
+
+            // in-place, so we use `done` to track the task done, prune others
+            if (done) return;
+
+
+            // go to fill the next empty pos
+            if (board[row][col] !== ".") return backtracking(...getNextPos(row, col));
+
+            for (const cand of candidates) {
+                board[row][col] = cand;
+                if (isLegal(row, col)) {
+                    backtracking(...getNextPos(row, col));
+                }
+                if (done) return;
+                board[row][col] = ".";
+            }
+        };
+
+        backtracking(0, 0);
+    }
+
+    // isValidSudoku(board: string[][]): boolean {
+    //     let state = new Set();
+    //     const side = 9;
+
+    //     for (let row = 0; row < side; row++) {
+    //         for (let column = 0; column < side; column++) {
+    //             const value = board[row][column];
+    //             if (value === ".") continue;
+    //             const rowRecord = `row ${row} value ${value}`;
+    //             const columnRecord = `column ${column} value ${value}`;
+    //             const boxRecord = `box ${Math.floor(row / 3)} / ${Math.floor(column / 3)} value ${value}`;
+
+    //             // console.log("state===" + state.has(rowRecord), "===" + state.has(columnRecord), "====" + state.has(boxRecord));
+    //             if (state.has(rowRecord) || state.has(columnRecord) || state.has(boxRecord)) {
+    //                 console.log("faaaaaaaaaa");
+    //                 return false;
+    //             }
+    //             // console.log("rowRecord===" + rowRecord);
+    //             // console.log("columnRecord===" + columnRecord);
+    //             // console.log("boxRecord===" + boxRecord);
+
+    //             state.add(rowRecord).add(columnRecord).add(boxRecord);
+    //         }
+    //     }
+
+    //     console.log("trrrrrrrrrr");
+
+    //     return true;
+    // }
+
+
+    private colArray = [];
     createOfRule() {
         let r = this.getRandom(0, 81);
+        let rText = this.getRandom(1, 9);
+        // console.log("===要加的行===" + this.sudokuTakeTheQuotient(r),
+        //     "===哪一行陣列===" + this.sudokuArray[this.sudokuTakeTheQuotient(r)],
+        //     "===要加的值===" + rText.toString());
+        // 在for一次0~8存到arr在比較
 
-        for (const iterator of this.sudokuArray[this.sudokuTakeTheQuotient(r)]) {
-            console.log("iterator=" + iterator);
+
+
+        if (this.node.children[r].getComponent(cc.Label).string == "" && this.sudokuArray[this.sudokuTakeTheQuotient(r)].indexOf(rText.toString()) == -1) {
+
+            // if (this.node.children[r].getComponent(cc.Label).string == "") {
+            this.sudokuArray[this.sudokuTakeTheQuotient(r)][this.sudokuTakeTheRemainder(r)] = rText.toString();
+            this.node.children[r].getComponent(cc.Label).string = rText.toString();
 
         }
-
-        console.log("if ===" + this.sudokuArray[this.sudokuTakeTheQuotient(r)].indexOf(this.sudokuTakeTheQuotient(r).toString()));
-
-        if (this.sudokuArray[this.sudokuTakeTheQuotient(r)].indexOf(this.sudokuTakeTheQuotient(r).toString()) == -1) {
-            if (this.node.children[r].getComponent(cc.Label).string == "") {
-                this.node.children[r].getComponent(cc.Label).string = this.getRandom(1, 9).toString();
-                this.sudokuArray[this.sudokuTakeTheQuotient(r)][this.sudokuTakeTheRemainder(r)] = this.node.children[r].getComponent(cc.Label).string;
-
-                console.log("this.sudokuArray==" + this.sudokuTakeTheQuotient(r),
-                    "==sudokuTakeTheQuotient=" + this.sudokuArray[this.sudokuTakeTheQuotient(r)],
-                    "string===" + this.node.children[r].getComponent(cc.Label).string);
-            }
-            // else {
-            //     this.createOfRule();
-            // }
+        else {
+            this.createOfRule();
         }
-        // else {
-        //     this.createOfRule();
-        // }
-        // else {
-        //     // console.log("else===" + this.sudokuArray[this.sudokuTakeTheQuotient(r)].indexOf(this.sudokuTakeTheQuotient(r).toString()));
 
-        // }
     }
 
     sudokuTakeTheQuotient(r: number): number {
