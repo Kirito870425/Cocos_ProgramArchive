7961e1c19beed76c6c2cfd0561a9f1efc9c3a305
Kirito870425 <dsa50163@gmail.com>
2022-08-17 22:31:48 +0800

    Save Old Project

--- work/assets/Scripts/IGSinterview.ts
+++ work/assets/Scripts/IGSinterview.ts
@@ -6,15 +6,17 @@ export default class IGSinterview extends cc.Component {
 
     protected start(): void {
         this.test4([1, 1, 2, 1, 1, 1], 1);
+        // this.test4([1, 2, 2, 4, 3, 2], 2);
         // this.test5([2, 2, 3, 4, 2]);
         // this.test1([1, 3, 5, 7, 9], [2, 4, 6, 8, 10]);
-        // this.test4([1, 2, 2, 4, 3, 2], 2);
     }
     // 最大的連續數
     test4(arr: Array<number>, key: number) {
         let temp = 0;
+        // let ansarr = [];
         for (let i = 0; i < arr.length; i++) {
             if (arr[i] == key && arr[i] == arr[i - 1] || arr[i] == arr[i + 1])
+                // ansarr.push(key);
                 temp++;
             else
                 temp = 0;
